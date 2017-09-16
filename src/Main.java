542d998d1bfaffa28d88bc4c406641d9ef82b171
veeral15 <31990490+veeral15@users.noreply.github.com>
2017-09-16 22:33:28 +0530

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -6,9 +11,40 @@ public class Main {
 		//Please check that your code works and has no 
 		//compilation problems before to submit
 		
-		StudentGroup sg=new StudentGroup(5);
-		sg.getStudents();
+		int sid=0;
+		String fn=null;
+		Date birthday=null;
+		double avgmrk=0.0;
+		
+		System.out.println("Enter the number of records you want to enter:");
+		Scanner s=new Scanner(System.in);
+		int num_rec=s.nextInt();
+		
+		StudentGroup sg=new StudentGroup(num_rec);
+		
+		for(int i=0;i<=num_rec;i++)
+		{
+			System.out.println("Enter the student ID");
+			sid=s.nextInt();
+			
+			System.out.println("Enter the student's Full Name (Eg: David Beckham)");
+			fn=s.nextLine();
+			
+			System.out.println("Enter the student Date Of Birth in yyyy-mm-dd format");
+			String next = s.next("[0-9]{4}.[0-9]{2}.[0-9]{2}");
+			try {
+				birthday = new SimpleDateFormat("yyyy.MM.dd").parse(next);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			
+			System.out.println("Enter the student's Average Marks");
+			avgmrk=s.nextFloat();
+			
+			Student st=new Student(sid,fn,birthday,avgmrk);
+		}
 	
+		
 	}
 
 }
